b1fa768b0c46b20e5b68c48f8cd371e541082f52
kodamulla <hansikavithma25@gmail.com>
2026-02-19 08:23:27 +0530

    update context

--- context/context.js
+++ context/context.js
@@ -329,3 +329,125 @@ export const VOTER_DAPP_PROVIDER = ({ children }) => {
       console.log(error);
     }
   };
+   const UPDATE_VOTER = async (updateVoter, image, pdf) => {
+    const {
+      _name,
+      _voterAddress,
+      _photograph,
+      _parentOrSpouseName,
+      _gender,
+      _dobOrAge,
+      _addressDetails,
+      _epicNumber,
+      _partNumberAndName,
+      _assemblyConstituencyNumberAndName,
+      _issuingAuthoritySignature,
+      _hologramAndBarcode,
+    } = updateVoter;
+
+    if (
+      !_name ||
+      !_voterAddress ||
+      !_photograph ||
+      !_parentOrSpouseName ||
+      !_gender ||
+      !_dobOrAge ||
+      !_addressDetails ||
+      !_epicNumber ||
+      !_partNumberAndName ||
+      !_assemblyConstituencyNumberAndName ||
+      !_issuingAuthoritySignature ||
+      !_hologramAndBarcode ||
+      !image ||
+      !pdf
+    )
+      return notifyError("Data Is Missing");
+    notifySuccess("Upadate Voter, kindly wait...");
+    setLoader(true);
+
+    const CONTRACT = await VOTING_CONTRACT();
+
+    const data = JSON.stringify({
+      _name,
+      _voterAddress,
+      _photograph,
+      _parentOrSpouseName,
+      _gender,
+      _dobOrAge,
+      _addressDetails,
+      _epicNumber,
+      _partNumberAndName,
+      _assemblyConstituencyNumberAndName,
+      _issuingAuthoritySignature,
+      _hologramAndBarcode,
+      image,
+      pdf,
+    });
+
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        data: data,
+        headers: {
+          pinata_api_key: `12334e89c69a413613e0`,
+          pinata_secret_api_key: `02779bdb49f3443d4501cf68c761b966fddbbe41f44a0905f674b8b558acb873`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      const url = `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`;
+      console.log(url);
+
+      const transaction = await CONTRACT.updateVoter(_name, url);
+
+      await transaction.wait();
+
+      notifySuccess("Successfully updated voter");
+      setLoader(false);
+      window.location.href = "/register-voter";
+    } catch (error) {
+      setLoader(false);
+      notifySuccess("Update failed, kindly connect to ellection commission");
+      console.log(error);
+    }
+  };
+
+  const UPDATE_CANDIDATE = async (updateCandidate, image, pdf) => {
+    const {
+      _name,
+      _nominationForm,
+      _affidavit,
+      _criminalAntecedents,
+      _assetsAndLiabilities,
+      _educationalQualifications,
+      _electoralRollEntry,
+      _securityDeposit,
+      _partyAffiliation,
+      _oathOrAffirmation,
+      _photographs,
+      _proofOfAge,
+      _proofOfAddress,
+      _panCardDetails,
+      _voterIdCardDetails,
+    } = updateCandidate;
+
+    if (
+      !_name ||
+      !_nominationForm ||
+      !_affidavit ||
+      !_criminalAntecedents ||
+      !_assetsAndLiabilities ||
+      !_educationalQualifications ||
+      !_electoralRollEntry ||
+      !_securityDeposit ||
+      !_partyAffiliation ||
+      !_oathOrAffirmation ||
+      !_photographs ||
+      !_proofOfAge ||
+      !_proofOfAddress ||
+      !_panCardDetails ||
+      !_voterIdCardDetails ||
+      !image ||
+      !pdf
+    )
