4fb32a2e0feb4a3f206d0070b07150387fb7e40a
kodamulla <hansikavithma25@gmail.com>
2026-02-20 10:30:24 +0530

    complete candidate detail page

--- pages/candidate-details.js
+++ pages/candidate-details.js
@@ -40,3 +40,69 @@ const candidateDetails = () => {
     GET_SINGLE_VOTER,
     INITIAL_CONTRACT_DATA,
   } = useContext(VOTING_DAPP_CONTEXT);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!router.isReady) return;
+
+      const items = await GET_SINGLE_CANDIDATE(router?.query.address);
+      setCandidate(items);
+
+      const allVotedVoter = await ALL_VOTERS_VOTED();
+      console.log(items);
+
+      const votingStatus = await INITIAL_CONTRACT_DATA();
+      setVotingTime(votingStatus);
+
+      const nowInMilliseconds = Date.now();
+      const nowInSeconds = Math.floor(nowInMilliseconds / 1000);
+      setCurrentVotingTime(nowInSeconds);
+
+      const address = await checkIfWalletIsConnected();
+
+      if (address) {
+        const user = await GET_SINGLE_VOTER(address);
+        setUser(user);
+        console.log(user);
+      }
+    };
+
+    fetchData();
+  }, [router.isReady]);
+
+  const approveCandidate = async (address, message) => {
+    await APPROVE_CANDIDATE(address, message);
+  };
+
+  const rejectCandidate = async (address, message) => {
+    await REJECT_CANDIDATE(address, message);
+  };
+  //
+  return (
+    <>
+      <Preloader />
+      <ScrollToTop />
+      <Cursor />
+      <Header />
+      <TeamDetail
+        candidate={candidate}
+        path={"candidate"}
+        handleClickApprove={approveCandidate}
+        handleClickReject={rejectCandidate}
+        GIVE_VOTE={GIVE_VOTE}
+        OWNER_ADDRESS={OWNER_ADDRESS}
+        address={address}
+        checkVote={checkVote}
+        user={user}
+        votingTime={votingTime}
+        currentVotingTime={currentVotingTime}
+      />
+      {loader && <Loader />}
+      <Footer />
+    </>
+  );
+};
+
+export default candidateDetails;
+
+
